d19009fd2e7556e137fb361e5dbd8532c6faf4b1
toobii346 <tobijoeleboka@gmail.com>
2026-03-11 11:00:48 +0000

    Day 1 Python code

--- Python-Projects/Day-1-hello-world/hello.py
+++ Python-Projects/Day-1-hello-world/hello.py
@@ -1,1 +1,14 @@
+# My first Python program
+print("Hello World!")
 
+# It Asks for user's name
+name = input("What is your name? ")
+
+# Prints a greeting
+print("Nice to meet you, " + name + "!")
+
+# Does a simple calculation
+number1 = 10
+number2 = 5
+result = number1 + number2
+print("10 + 5 = " + str(result))
